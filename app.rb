becd56bc3e7587ddc2211cb8efc6af3a87b58a65
Jake Dahn <admin@jakedahn.com>
2011-04-27 11:52:56 -0700

    porting all of the current pages over

--- app.rb
+++ app.rb
@@ -11,5 +11,30 @@ module SinatraApp
       haml :index
     end
 
+    # compute pages
+    
+    get '/compuate_dashboard' do
+      haml :compute_dashbaord
+    end
+
+    get '/manage_instances' do
+      haml :manage_instances
+    end
+
+    get '/manage_volumes' do
+      haml :manage_volumes
+    end
+
+    get '/manage_security' do
+      haml :manage_security
+    end
+
+    get '/manage_vpn' do
+      haml :manage_vpn
+    end
+
+
+
+
   end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,19 @@
 $(function(){
-  $("#wrapper")
+  $('input#table_search').quicksearch('table tbody.main tr');
+  
+  $("#more").hide()  
+  $("#page_header a.more_link").click(function(e){  
+    $("#more").fadeToggle("slow")
+    e.preventDefault();
+  })
+  
+  $("#health_table tr").hide()
+  $("#health_table tr:first").show()
+  $("#health_table tr.odd, #health_table tr.even").show()
+  
+  $("#health_table .more_link").click(function(e){
+    $(this).parent().parent().nextUntil(".even, .odd").fadeToggle("slow")
+    e.preventDefault();
+  })
+  
 })
